f429287b8db003a02502d18c1b095aa6f370ad6f
luanngominh <ngominhluanbox@gmail.com>
2020-08-28 16:52:50 +0700

    use api 23 insted

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-IMAGE_VERSION="api-30"
+IMAGE_VERSION="api-23"
 
 docker build -t luanngominh/android-flutter:${IMAGE_VERSION} .
 docker push luanngominh/android-flutter:${IMAGE_VERSION}
